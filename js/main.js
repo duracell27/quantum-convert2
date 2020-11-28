bc1511d5ad665be500f605993a41c79f5b0e790a
duracell27 <volodymyrshmidt@gmail.com>
2020-11-28 17:26:48 +0200

    2 lesson ready

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
-const selectElement = document.querySelector('[data-select]');
-const selectReal = document.querySelector('#category');
+const selectAllElements = document.querySelectorAll('[data-select]');
 
-selectElement.addEventListener('click', function (e) {
-	if (e.target.hasAttribute('data-select-item')) {
-		const itemTitle = e.target.dataset.selectItem;
-		e.target.closest('[data-select]').querySelector('[data-select-title]').innerText = itemTitle;
-		e.target.closest('[data-select]').querySelector('.header-select__dropdown').classList.toggle('hidden');
-		selectReal.value = itemTitle;
-	} else {
-		this.querySelector('.header-select__dropdown').classList.toggle('hidden');
-	}
+selectAllElements.forEach(function (element) {
+	element.addEventListener('click', function (e) {
+		const realSelect = this.nextElementSibling;
+		if (e.target.hasAttribute('data-select-item')) {
+
+			const itemTitle = e.target.dataset.selectItem;
+			this.querySelector('[data-select-title]').innerText = itemTitle;
+
+
+			this.querySelector('.header-select__dropdown').classList.toggle('hidden');
+			realSelect.value = itemTitle;
+
+		} else {
+			this.querySelector('.header-select__dropdown').classList.toggle('hidden');
+		}
+	})
 })
+
+// selectElement.addEventListener('click', function (e) {
+// 	if (e.target.hasAttribute('data-select-item')) {
+// 		const itemTitle = e.target.dataset.selectItem;
+// 		e.target.closest('[data-select]').querySelector('[data-select-title]').innerText = itemTitle;
+// 		e.target.closest('[data-select]').querySelector('.header-select__dropdown').classList.toggle('hidden');
+// 		selectReal.value = itemTitle;
+// 	} else {
+// 		this.querySelector('.header-select__dropdown').classList.toggle('hidden');
+// 	}
+// })
